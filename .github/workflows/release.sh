b166326aa505144b65a777cd48372e6d0c657ab1
Pim Vermeer <pim.vermeer@gmail.com>
2026-01-01 12:45:31 +0000

    chore(ci): add deps

--- .github/workflows/release.sh
+++ .github/workflows/release.sh
@@ -5,7 +5,7 @@ set -e
 echo -e "\n==== Running release package ====\n"
 
 sudo apt-get update -y
-sudo apt-get install -y libglib2.0-dev
+sudo apt-get install -y flatpak-builder libglib2.0-dev gtk4-devel libadwaita-devel
 cargo run --bin=release
 
 echo -e "\n==== Done ====\n"
