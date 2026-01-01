a9184790a0a58769e2d4c124c7c192dbd9323732
Pim Vermeer <pim.vermeer@gmail.com>
2026-01-01 12:58:38 +0000

    chore(ci): fix deps

--- .github/workflows/release.sh
+++ .github/workflows/release.sh
@@ -5,7 +5,13 @@ set -e
 echo -e "\n==== Running release package ====\n"
 
 sudo apt-get update -y
-sudo apt-get install -y flatpak-builder libglib2.0-dev gtk4-dev libadwaita-dev
+sudo apt-get install -y \
+    flatpak-builder \
+    build-essential \
+    libglib2.0-dev \
+    libgtk-4-dev \
+    libadwaita-1-dev
+
 cargo run --bin=release
 
 echo -e "\n==== Done ====\n"
